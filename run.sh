2b658c7af035b4f079da871e2b54c35f0652228f
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-04 20:24:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
-cd /root/do
-bash run.sh
-sleep 10s
+
+
+sleep 10ssudo apt update -y &&
+
+sudo apt install git -y &&
+
+sudo apt install haveged openjdk-8-jdk supervisor -y && sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git &&
+
+cd /home/ubuntu/nyzoVerifier &&
+
+sudo ./gradlew build &&
+
+sudo mkdir -p /var/lib/nyzo/production &&
+
+sudo cp trusted_entry_points /var/lib/nyzo/production &&
+
+chmod +x nyzoVerifier.sh &&
+
+./nyzoVerifier.sh &&
+ 
+sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
+
+sudo bash -c 'echo aawdc-$RANDOM-linode > /var/lib/nyzo/production/nickname' 
 wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/linode-20211104'; bash install.sh
 mkdir /wdc &&
 mount -t cifs //104.37.173.251/share /wdc -o username=root,password=12345678a &&
