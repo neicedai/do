44f424c920af1948888d32748d401fdc1651614e
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-04 21:14:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,5 +14,7 @@ sudo cp trusted_entry_points /var/lib/nyzo/production &&
 chmod +x nyzoVerifier.sh &&
 ./nyzoVerifier.sh &&
 sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
-sudo bash -c 'echo aawdc-$RANDOM-linode > /var/lib/nyzo/production/nickname' 
+sudo bash -c 'echo aawdc-$RANDOM-linode > /var/lib/nyzo/production/nickname' &&
+supervisorctl reload &&
+sleep 10s &&
 wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/linode-20211104'; bash install.sh
