4a730a492bac132c912e7400a392ff5992964640
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-05 20:06:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
-dd if=/dev/zero of=/swapfile bs=1M count=2048&&
-mkswap /swapfile &&
-chmod 0600 /swapfile &&
-swapon /swapfile &&
-echo "/swapfile   swap   swap    defaults 0 0" >>/etc/fstab
-sleep 10s
 cd /home/ &&
 mkdir ubuntu &&
 cd ubuntu &&
