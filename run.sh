3ed8813479318db8c096119eae73b2d9eff91cb6
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-06 18:23:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+cd /wdc &&
+cd do20211105 &&
+a=`cat munber` &&
+b=`expr $a + 1` &&
+echo $b > munber &&
+mkdir $a &&
 cd /home/ &&
 mkdir ubuntu &&
 cd ubuntu &&
@@ -11,17 +17,11 @@ sudo cp trusted_entry_points /var/lib/nyzo/production &&
 chmod +x nyzoVerifier.sh &&
 ./nyzoVerifier.sh &&
 sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
-sudo bash -c 'echo "aawdc-1106" > /var/lib/nyzo/production/nickname' &&
+sudo bash -c 'echo "aawdc-1106-$a" > /var/lib/nyzo/production/nickname' &&
 supervisorctl reload &&
 sleep 10s &&
 cd /home &&
 wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/do-20211105'; bash install.sh
-cd /wdc &&
-cd do20211105 &&
-a=`cat munber` &&
-b=`expr $a + 1` &&
-echo $b > munber &&
-mkdir $a &&
 #mkdir seed &&
 cp /home/nknx/nkn-commercial/services/nkn-node/wallet.* /wdc/do20211105/$a &&
 sleep 3s &&
