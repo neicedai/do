310aef13bce4fe2214da90dbaeabb1b9bd449126
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-08 22:18:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd /wdc &&
-cd do20211105 &&
+cd do20211108 &&
 a=`cat munber` &&
 b=`expr $a + 1` &&
 echo $b > munber &&
@@ -17,14 +17,14 @@ sudo cp trusted_entry_points /var/lib/nyzo/production &&
 chmod +x nyzoVerifier.sh &&
 ./nyzoVerifier.sh &&
 sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
-sudo bash -c 'echo "aawdc-1106-'$a'" > /var/lib/nyzo/production/nickname' &&
+sudo bash -c 'echo "aawdc-1108-'$a'" > /var/lib/nyzo/production/nickname' &&
 supervisorctl reload &&
 sleep 10s &&
 cd /home &&
 wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/do-20211105'; bash install.sh
 #mkdir seed &&
-cp /home/nknx/nkn-commercial/services/nkn-node/wallet.* /wdc/do20211105/$a &&
+cp /home/nknx/nkn-commercial/services/nkn-node/wallet.* /wdc/do20211108/$a &&
 sleep 3s &&
 wget -qO- http://ipecho.net/plain >> /root/temp &&
 echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /root/seed &&
-cp /root/seed /wdc/do20211105/seed/$a
+cp /root/seed /wdc/do20211108/seed/$a
