441fd27e0a66e9b537d4570f55acf8e5996cae20
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-09 20:20:27 +0800

    Update and rename runbak.sh to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 cd /wdc &&
-cd do20211108 &&
+###gai-1
+cd do1109 &&
 a=`cat munber` &&
 b=`expr $a + 1` &&
-echo $b > munber &&
+echo $b > num &&
 mkdir $a &&
 cd /home/ &&
 mkdir ubuntu &&
@@ -17,14 +18,17 @@ sudo cp trusted_entry_points /var/lib/nyzo/production &&
 chmod +x nyzoVerifier.sh &&
 ./nyzoVerifier.sh &&
 sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
-sudo bash -c 'echo "aawdc-1108-'$a'" > /var/lib/nyzo/production/nickname' &&
+###gai-2
+sudo bash -c 'echo "aawdc-1109-'$a'" > /var/lib/nyzo/production/nickname' &&
 supervisorctl reload &&
 sleep 10s &&
 cd /home &&
-wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/do-20211105'; bash install.sh
-#mkdir seed &&
-cp /home/nknx/nkn-commercial/services/nkn-node/wallet.* /wdc/do20211108/$a &&
+###gai-3
+wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/do-20211109'; bash install.sh
+###gai-4
+cp /home/nknx/nkn-commercial/services/nkn-node/wallet.* /wdc/do1109/$a &&
 sleep 3s &&
 wget -qO- http://ipecho.net/plain >> /root/temp &&
 echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /root/seed &&
-cp /root/seed /wdc/do20211108/seed/$a
+###gai-5
+cp /root/seed /wdc/do1109/seed/$a
