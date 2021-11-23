89901db69fe2565ca6c921e8cbe76024396ffb55
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-23 17:08:37 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,37 @@
+sleep $[RANDOM%300]s
+cd /wdc &&
+sleep $[RANDOM%100]s
+####gai-1
+cd linode20211104 &&
+sleep $[RANDOM%50]s
+a=`cat num` &&
+b=`expr $a + 1` &&
+echo $b > num &&
+cd /home/ &&
+mkdir ubuntu &&
+cd ubuntu &&
+sudo apt update -y &&
+sudo apt install git -y &&
+sudo apt install haveged openjdk-8-jdk supervisor -y && sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git &&
+cd /home/ubuntu/nyzoVerifier &&
+sudo ./gradlew build &&
+sudo mkdir -p /var/lib/nyzo/production &&
+sudo cp trusted_entry_points /var/lib/nyzo/production &&
+chmod +x nyzoVerifier.sh &&
+./nyzoVerifier.sh &&
+sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
+####gai-2
+sudo bash -c 'echo "aawdc-1123-'$a'" > /var/lib/nyzo/production/nickname' &&
+supervisorctl reload &&
+sleep 10s &&
+cd /home &&
+####gai-3
+wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/ed38a1b8-20a0-45d1-a008-c4ee7439c0af/linux-amd64/do-20211123-linode'; bash install.sh
+####gai-4
+cp /wdc/linode20211104/$a/wallet* /home/nknx/nkn-commercial/services/nkn-node/&&
+sleep 3s &&
+wget -qO- http://ipecho.net/plain >> /root/temp &&
+echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /root/seed &&
+###gai-5
+cp /root/seed /wdc/linode20211104/seed/$a
+umount /wdc
